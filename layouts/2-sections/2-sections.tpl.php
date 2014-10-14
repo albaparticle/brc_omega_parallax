c5659ff0007093947a6273a254ba107ca8d49d21
palagpatski <albaparticle2005@yahoo.com>
2014-10-14 12:44:57 +0800

    Added a custom template for a panel

---
+++ layouts/2-sections/2-sections.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Plain (naked) layout template, simply printing the content area.
+ */
+?>
+
+<div id="group4" class="parallax__group">
+  <div class="parallax__layer parallax__layer--base">
+    <?php print $content['content']; ?>
+  </div>
+  <div class="parallax__layer parallax__layer--back">
+  </div>
+</div>
