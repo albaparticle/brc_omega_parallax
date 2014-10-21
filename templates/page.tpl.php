37fa7cfe7b95dc789db72ce151120de15a47fb98
palagpatski <albaparticle2005@yahoo.com>
2014-10-21 10:39:24 +0800

    Maintenance page themeing and page modifications

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -97,24 +97,26 @@
   </header>
 
   <div class="l-main">
+  	<?php print render($page['highlighted']); ?>
+    <?php print $breadcrumb; ?>
+	<?php print $messages; ?>
+    <?php print render($tabs); ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+
     <div class="l-content" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
+        <h1 class="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div>
+
+    <?php print $feed_icons; ?>
 
     <?php print render($page['sidebar_first']); ?>
     <?php print render($page['sidebar_second']); ?>
